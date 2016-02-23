5e0589f9dbcaf155971867cf56588e08a17f2d9c
rodolfoperez200 <rodolfo.perez200@hotmail.com>
2016-02-23 20:00:25 -0300

    Dynamic template with React-template (user .rt files)

--- my-proyect/cfg/defaults.js
+++ my-proyect/cfg/defaults.js
@@ -47,6 +47,10 @@ function getDefaultModules() {
       {
         test: /\.(png|jpg|gif|woff|woff2)$/,
         loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.rt/,
+        loader: 'react-templates-loader'
       }
     ]
   };
